55249d11892b320d93f5ea62c7247bd28c973999
RsanabriaZ <Owner@Rene.local>
2015-01-26 21:18:22 -0600

    tipo carro timestamp false

--- app/models/TipoCarro.php
+++ app/models/TipoCarro.php
@@ -4,6 +4,7 @@
         // use SoftDeletingTrait;
         public $errors;
         protected $table = 'tipoCarros';
+        public $timestamp = false;
         // protected $dates = ['deleted_at'];
         // protected $perPage = 5;
         protected $fillable = ['tipo'];
